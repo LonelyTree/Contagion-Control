78d7fae90b08990c41673e0ffae039e066259cf1
Alex Hughes <alexhugh121@gmail.com>
2019-04-02 15:20:04 -0700

    Able to Place Div Ontop of Google Maps.

--- testing/js/main.js
+++ testing/js/main.js
@@ -1,386 +1,96 @@
 
-        // This example creates circles on the map, representing populations in North
-        // America.
+    // This example creates circles on the map, representing populations in North
+    // America.
 
-        // First, create an object containing LatLng and population for each city.
-src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBR0S-krTPd4hZ27aoWsRVGPvZ-LDY98zc&callback=initMap"
-var citymap = {
-chicago: {
-    center: { lat: 41.878, lng: -87.629 },
-},
-newyork: {
-    center: { lat: 40.714, lng: -74.005 },
-},
-losangeles: {
-    center: { lat: 34.052, lng: -118.243 },
-},
-vancouver: {
-    center: { lat: 49.25, lng: -123.1 },
-}
-};
+    // First, create an object containing LatLng and population for each city.
+    var citymap = [
+    {
+        name: 'chicago',
+        center: {lat: 41.878, lng: -87.629},
+        area: 240
+    },
+    {   name: "newyork",
+        center: {lat: 40.714, lng: -74.005},
+        area: 302.6
+    },
+    {   name: "losangeles",
+        center: {lat: 34.052, lng: -118.243},
+        area: 468.57
+    },
+    {   name: "vancouver",
+        center: {lat: 49.25, lng: -123.1},
+        area: 1000
+    },
+    {   name: "houston",
+        center: {lat: 29.76, lng: -95.36},
+        area: 599.6
+    },
+    {   name: "toyko",
+        center: {lat: 35.68, lng: 139.69},
+        area: 1000
+    },
+    {   name: "jakarta",
+        center: {lat: -6.17, lng: 106.86},
+        area: 600.7	
+    },
+    {   name: "new orleans",
+        center: {lat: 29.95, lng: -90.07},
+        area: 169.4
+    },
+    {   name: "delhi",
+        center: {lat: 28.63, lng: 77.23},
+        area: 400
+    },
+    {   name: "manilla",
+        center: {lat: 14.59, lng: 120.98},
+        area: 420
+    },
+    {   name: "seoul",
+        center: {lat: 37.56, lng: 126.97},
+        area: 800
+    },
+    {   name: "beijing",
+        center: {lat: 39.92, lng: 116.46},
+        area: 1000
+    }
+];
 
-function initMap() {
-// Create the map.
-var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10.25,
-    center: { lat: 34.052, lng: -118.243  },
-    mapTypeId: 'terrain'
-});
+const selectedCity = citymap[Math.floor(Math.random() * citymap.length)]
 
-// Construct the circle for each value in citymap.
-// Note: We scale the area of the circle based on the population.
-for (var city in citymap) {
-    // Add the circle for this city to the map.
-    var cityCircle = new google.maps.Circle({
+    // function initMap() {
+    // // Create the map.
+    // var map = new google.maps.Map(document.getElementById('map'), {
+    //     zoom: (citymap[selectedCityArea].area <= 250) ? 14 : 10.25,
+    //     center: selectedCityLonLat,
+    //     mapTypeId: 'terrain'
+    // });
+    function initMap() {
+        // Create the map.
+        var map = new google.maps.Map(document.getElementById('map'), {
+            // zoom: (selectedCity.area <= 250) ? 14 : 10.25,
+            zoom: (selectedCity.area <= 250) ? 13 : (selectedCity.area <= 350) ? 12 : (selectedCity.area <= 500) ? 10.25 : (selectedCity.area <= 750) ? 9.25 : (selectedCity.area <= 1000) ? 9 : 10.25,
+            center: selectedCity.center,
+            mapTypeId: 'terrain'
+        });
+    // for (var area in citymap){
+    //     if(area <=250){
+    //         map.zoom: 14
+    //     }
+    // }
+
+    // Construct the circle for each value in citymap.
+    // Note: We scale the area of the circle based on the population.
+    
+        // Add the circle for this city to the map.
+        var cityCircle = new google.maps.Circle({
         strokeColor: '#FF0000',
         strokeOpacity: 0.8,
         strokeWeight: 2,
         fillColor: '#FF0000',
         fillOpacity: 0.35,
         map: map,
-        center: citymap[city].center,
-        radius: Math.sqrt(citymap[city].population) * 100
-    });
-}
-}
-
-const Css = require('json-to-css')
-const json = require([
-    {
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#f5f5f5"
-            }
-        ]
-    },
-    {
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#616161"
-            }
-        ]
-    },
-    {
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "color": "#f5f5f5"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.locality",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#8f0000"
-            },
-            {
-                "weight": 8
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.locality",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "color": "#68611a"
-            },
-            {
-                "lightness": 75
-            },
-            {
-                "weight": 4
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.neighborhood",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#fdfffe"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#eeeeee"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#757575"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.government",
-        "elementType": "labels.text",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.government",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#8f0000"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.government",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "color": "#bdb126"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.medical",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.medical",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "color": "#ff2600"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.medical",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#8f0000"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.medical",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "color": "#423e04"
-            },
-            {
-                "lightness": 75
-            },
-            {
-                "visibility": "on"
-            },
-            {
-                "weight": 6
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#969696"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#c1c1c1"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#dadada"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#065290"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#a9a9a9"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#e5e5e5"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#5e5e5e"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.station.airport",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#fffb00"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.station.airport",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "color": "#ff2600"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#c9c9c9"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#b1d9fb"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#9e9e9e"
-            }
-        ]
+        center: selectedCity.center,
+        radius: (selectedCity.area *7) 
+        });
     }
-])
-const css = Css.of(json)
-console.log(css)
+    
